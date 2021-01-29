5b4dbd184578715d38df8fc523ccce66a44b022c
Nestor <nestoramichetti@neuronic.com.ar>
2021-01-29 00:22:34 -0300

    Subiendo todo a github

--- src/main/java/com/nestor/electromecanica/entity/Cliente.java
+++ src/main/java/com/nestor/electromecanica/entity/Cliente.java
@@ -12,15 +12,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name="clientes")
-@Data
+
 public class Cliente implements Serializable{
 
-
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -36,6 +34,55 @@ public class Cliente implements Serializable{
 	public Cliente() {
 		this.motores=new ArrayList<Motor>();
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public List<Motor> getMotores() {
+		return motores;
+	}
+
+	public void setMotores(List<Motor> motores) {
+		this.motores.addAll(motores);
+	}
+
 
 	
 	
